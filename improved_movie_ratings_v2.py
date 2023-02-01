ce8b358a3ee7c208eb25c35aa0250b70be484f77
Bakar <bakarhs123@gmail.com>
2023-02-01 16:21:50 +0000

    started v2

---
+++ improved_movie_ratings_v2.py
@@ -0,0 +1,43 @@
+# user_prompt = True
+#
+# while user_prompt:
+#     age = input("What is your age?")
+#     if age.isdigit() and int(age) < 117 and int(age) > 0:
+#         user_prompt = False
+#
+#     else:
+#         print("Please enter your answer in digits and bellow 117. If your child is under 1 yrs old we advise we do not advise bringing them into the movie.")
+#
+# if int(age) < 12:
+#     print('U and PG films are available')
+# elif int(age) < 15:
+#     print('U, PG and 12 films are available')
+# elif int(age) < 18:
+#     print("U, PG, 12 and 15 films are available")
+# else:
+#     print('all films are available')
+def movie_rating(age):
+
+    if age < 12:
+        return ('U and PG films are available')
+    elif age < 15:
+        return ('U, PG and 12 films are available')
+    elif age < 18:
+        return("U, PG, 12 and 15 films are available")
+    else:
+        return ('all films are available')
+
+user_prompt = True
+
+while user_prompt:
+    age = input("What is your age?")
+    if age.isdigit() and int(age) < 117 and int(age) > 0:
+       user_prompt = False
+    else:
+        return ("Please enter your answer in digits and bellow 117. If your child is under 1 yrs old we advise we do not advise bringing them into the movie.")
+
+movie_rating(age)
+
+
+
+
